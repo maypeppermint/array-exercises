58eded58178133ca5e4df188b17fe579b904fd81
Veronica Wu <maypeppermint@gmail.com>
2021-04-27 16:35:47 +0200

    Solution

--- uppgift1.js
+++ uppgift1.js
@@ -7,3 +7,9 @@ Skriv sedan ut alla platser i konsollen genom att definera arrayens plats med et
 alltså inte en variabel. Du måste göra 5 stycken console.log.
 
 */
+let myArray = ["Apa", "Apa", "Apa", "Apa", "Apa"];
+console.log(myArray.indexOf("Apa",0));
+console.log(myArray.indexOf("Apa",1));
+console.log(myArray.indexOf("Apa",2));
+console.log(myArray.indexOf("Apa",3));
+console.log(myArray.indexOf("Apa",4));
